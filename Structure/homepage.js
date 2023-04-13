727cb1c97165cf37d8786694830b01a00c30a595
RexeonNova <74758948+RexeonNova@users.noreply.github.com>
2023-04-13 19:27:06 +0700

    Add click to homepage

--- Structure/homepage.js
+++ Structure/homepage.js
@@ -7,12 +7,51 @@ let geoGenerator = d3.geoPath()
   .projection(projection)
   .context(context);
   
-d3.json("ne_110m_admin_0_countries.json").then(function(geojson) {
-	projection.fitExtent([[20, 20], [2000, 1000]], geojson);
+let country = {
+	clickedLocation: null
+};
+let geojson = {};
+ 
+d3.json("ne_110m_admin_0_countries.json").then(function(json) {
+	geojson = json;
+	initialize();
+	update();
+});
 
+function update() {
+	context.clearRect(20, 20, 2000, 1000);
+	projection.fitExtent([[20, 20], [2000, 1000]], geojson);
+	/*
   	context.lineWidth = 0.5;
   	context.strokeStyle = '#888';
   	context.beginPath();
 	geoGenerator({type: 'FeatureCollection', features: geojson.features});
 	context.stroke();
-});
+	*/
+	geojson.features.forEach(function(d) {
+		context.lineWidth = 0.5;
+  		context.strokeStyle = '#888';
+		context.beginPath();
+		if (country.clickedLocation == null) {
+			context.strokeStyle = '#888';
+		} else {
+			context.strokeStyle = country.clickedLocation && d3.geoContains(d, country.clickedLocation) ? '#888' : 'red';
+		}
+		geoGenerator(d);
+		context.stroke();
+		//context.fill()
+	});
+}
+
+function handleClick(e) {
+	let pos = d3.pointer(e, this);
+	console.log(pos);
+	country.clickedLocation = projection.invert(pos);
+	console.log(country.clickedLocation);
+	update();
+}
+
+function initialize() {
+	d3.select('#map canvas')
+		.on('click', handleClick);
+}
