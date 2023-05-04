30fef054a0a9b9245a6c2ccb80a64be4757058b6
RexeonNova <74758948+RexeonNova@users.noreply.github.com>
2023-05-04 18:49:04 +0700

    mouseover + click

--- Structure/homepage.js
+++ Structure/homepage.js
@@ -7,6 +7,8 @@ let geoGenerator = d3.geoPath().projection(projection).context(context);
 
 let country = {
   clickedLocation: null,
+  code: null,
+  data: null
 };
 let geojson = {};
 
@@ -17,11 +19,11 @@ d3.json("http://127.0.0.1:8080/Country/map.geojson").then(function (json) {
 });
 
 function update() {
-  context.clearRect(20, 20, 2000, 1000);
+  context.clearRect(20, 20, 1000, 400);
   projection.fitExtent(
     [
       [20, 20],
-      [2000, 1000],
+      [1000, 500],
     ],
     geojson
   );
@@ -43,21 +45,35 @@ function update() {
         country.clickedLocation && d3.geoContains(d, country.clickedLocation)
           ? "red"
           : "#888";
+        if (country.clickedLocation && d3.geoContains(d, country.clickedLocation)) {
+			country.code = d.properties["sov_a3"];
+			country.data = d;
+		}
     }
     geoGenerator(d);
     context.stroke();
     //context.fill()
   });
+  
+  if (country.data != null)
+  	d3.select("text.name").text(country.data.properties["sovereignt"]);
 }
 
 function handleClick(e) {
   let pos = d3.pointer(e, this);
-  console.log(pos);
+  //console.log(pos);
   country.clickedLocation = projection.invert(pos);
-  console.log(country.clickedLocation);
+  //console.log(country.clickedLocation);
   update();
 }
 
 function initialize() {
-  d3.select("#map canvas").on("click", handleClick);
+  d3.select("#map canvas").on("mousemove", handleClick);
+  d3.select("#map canvas").on("click", function(e) {
+	  if (country.code != null) {
+		  window.location.href = "template.html?country=" + country.code;
+	  }
+  })
+  d3.select("svg.detail").append("g").attr("class", "name").attr("transform", "translate(20,20)");
+  d3.select("g.name").append("text").attr("class", "name");
 }
