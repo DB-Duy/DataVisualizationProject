897208286b667988a92bbc858eace8fcfafdca1d
DBDuy <dbduy2001@gmail.com>
2023-05-21 12:13:14 +0700

    updated line chart

--- Structure/app.js
+++ Structure/app.js
@@ -43,7 +43,7 @@ const DrawGraph = (data) => {
   });
   // console.log(inflationRates);
 
-  const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+  const margin = { top: 20, right: 20, bottom: 50, left: 50 };
   const width = 600 - margin.left - margin.right;
   const height = 400 - margin.top - margin.bottom;
 
@@ -77,12 +77,63 @@ const DrawGraph = (data) => {
     .call(xAxis);
 
   svg.append("g").call(yAxis);
+  svg
+    .append("text")
+    .attr(
+      "transform",
+      "translate(" + width / 2 + "," + (height + margin.top + 20) + ")"
+    )
+    .style("text-anchor", "middle")
+    .text("Year");
+
+  svg
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - height / 2)
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Inflation Rate");
+
+  const tooltip = d3
+    .select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("pointer-events", "none")
+    .style("background-color", "rgba(0, 0, 0, 0.8)")
+    .style("color", "#fff")
+    .style("padding", "8px")
+    .style("font-size", "12px")
+    .style("border-radius", "4px")
+    .style("opacity", 0);
+
+  svg
+    .selectAll(".dot")
+    .data(inflationRates)
+    .enter()
+    .append("circle")
+    .attr("class", "dot")
+    .attr("cx", (d) => x(d[0]))
+    .attr("cy", (d) => y(d[1]))
+    .attr("r", 4)
+    .attr("fill", "steelblue")
+    .on("mouseover", (event, d) => {
+      // Show the tooltip
+      tooltip.style("opacity", 1);
+      tooltip
+        .html(`Year: ${d[0]}<br>Inflation Rate: ${d[1]}`)
+        .style("left", event.pageX + 10 + "px")
+        .style("top", event.pageY - 10 + "px");
+    })
+    .on("mouseout", () => {
+      // Hide the tooltip
+      tooltip.style("opacity", 0);
+    });
 
   const line = d3
     .line()
     .x((d) => x(d[0]))
     .y((d) => y(d[1]));
-
   svg
     .append("path")
     .datum(inflationRates)
